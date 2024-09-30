d917f19df80c39e7133dd581e7fe3934bf7fbcda
ZephyRemix <hunyaochong@gmail.com>
2024-09-30 12:13:57 +0800

    Implemented capability to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      # flash[:success] = "Object was successfully updated"
+      redirect_to @article
+    else
+      # flash[:error] = "Something went wrong"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
